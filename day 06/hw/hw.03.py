999f58923ce4eb49f4ac851a0463606b7d5d707d
barbare8 <barbaretsivilashvili@gmail.com>
2025-03-01 01:31:03 +0400

    commit

---
+++ day 06/hw/hw.03.py
@@ -0,0 +1,7 @@
+#  ახსენით რა არის camel-case, case-sensitive, snake-scase
+camel-case
+# camel-caსე-ში სიტყვების შეერთება შეგვიძლია და წინადადების მეორე სიტყვის პირველი ასო გავზარდოთ
+# case-sansitive არის პაიტონის ენა
+snake_case
+# snake_case-ში სიტყვებს შუაში ქვვედა ტირეები უნდა დავწეროთ
+
---
+++ day 06/hw/hw.04.py
@@ -0,0 +1,17 @@
+# რა არის debuging? ასევე გაასწორე ეს Eror ები:
+debaging
+# debaging არის კოდში ერორების გასწორება
+1)
+name = 'zura'
+print(name)
+2)
+age - 13
+print(age)
+3)
+Hello_world = "Hello"
+print(hello_world)
+4)
+name2 = 'nika'
+print(name2)
+year_2025
+print(year)
