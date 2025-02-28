44ea00c0a79acc02fe3f17c9bb7cee6a484e7e26
barbare8 <barbaretsivilashvili@gmail.com>
2025-02-28 23:59:11 +0400

    commit

---
+++ day 05/hw/day 06/cw/cw.py
@@ -0,0 +1,26 @@
+snake-case
+# snake-case არის ერთი დიდი გველი და სნეიკ-ქეისში არ შეიძლება ადგილის გამოტოვება
+case-sansitive
+# case-sensitiv არის აქლემი მასში მეორესიტყვას დიდი ასოთი ვწერთ
+# მაგალითები
+goa_ is_ the_ best
+group_ 62_ is_ the_ best
+batoni_ andria_ kobaxiize_ is_ the_ best
+barbareTSIVILASHVIILI
+anaPUTURIZE
+gabrielMAXUASHVILI
+)
+Name = 'nika',აქ შეცდომა არის ის რომ ნეიმი პრინტში პატარა ასოთი წერია
+print(name)
+2)
+goa best = 100 აქ გოა ბესთში _ საჭიროა
+print(goa best)
+3)
+name - 'mate' ადგილი პრინტში მ დიდი ასოთი წერია
+print(naMe)
+4)
+number- 10
+print{number)
+5)
+goBaest = "100%)
+print(goaBest)
---
+++ day 06/day 06/cw/py.py
@@ -0,0 +1,6 @@
+#  <!-- მომხმარებელს შემატანინე, ასაკი, გვარი, სახელი, დაბადების თარიღი
+name = input("hav old are you?")
+print(name)
+name = input("wat is your surname?")
+print(name)
+name = input("wat is your data of birth?")
