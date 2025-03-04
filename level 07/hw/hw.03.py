5ffd9e504bd8e2b167a6b379542263b9bb2ad115
barbare8 <barbaretsivilashvili@gmail.com>
2025-03-04 19:53:44 +0400

    commit

---
+++ level 07/hw/hw.03.py
@@ -0,0 +1,4 @@
+#  შექმენით საიტი გოაზე აქამდე ნასწავლი მასალით
+<h1>goa is the best</h1>
+<p>iths is the best academi</p>
+<button>click my if you love goa<button/>
---
+++ level 07/hw/hw.04.py
@@ -0,0 +1,7 @@
+# შექმენით საიტი მანქანებზე გამოიყენეთ მინიმუმ 6 img 6 button 6 p
+<img src='c:/user/barbareDesktop/manqana.jpg
+< img src='c:/user/barbareDesktop/manqana.jpg
+< img src='c:/yser/barbareDesktop/manqana.jpg
+< img src='c:/user/barbareDesktop/manqana.jpg
+< img src='c:/user/barbareDesktop/manqana.jpg
+< img src='c:/user/barbareDesktop/manqana.jpg
