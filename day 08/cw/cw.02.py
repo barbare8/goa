51b078376f71c2f081ab62536d71345cff6de89c
barbare8 <barbaretsivilashvili@gmail.com>
2025-03-08 01:50:59 +0400

    commit

---
+++ day 08/cw/cw.02.py
@@ -0,0 +1,11 @@
+#  2) შექმენით 5 ცვლადი და შეინახეთ სხვადასხვა მონაცემთა ტიპები. შემდეგ კი შეამოწმეთ თითოეული ცვლადი რომელ მონაცემთა ტიპს ინახავს და გამოიტანეთ შედეგი ეკრანზე.
+# print(type(group 62 is the best))
+# print(type(misis lika is the best))
+# print(type(goa is the best))
+# print(type(my cosune is the best))
+# print(type(evrione is the best))
+
+x = 9
+y = 3
+
+print(x / y)
---
+++ day 08/cw/cw.py
@@ -0,0 +1,3 @@
+#  1) კომენტარის სახით ახსენით თუ რას აკეთებს type() ფუნქცია.
+type
+# tipe amowmebs cvladSi seamowmos tu sworad weria is
