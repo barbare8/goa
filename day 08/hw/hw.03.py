0dc8d2300f30e726b8508328d5b7c0c8471e68f8
barbare8 <barbaretsivilashvili@gmail.com>
2025-03-14 00:05:02 +0400

    commit

--- day 08/hw/hw.03.py
+++
@@ -1,5 +0,0 @@
-# 3) კომენტარის სახით ჩამოწერეთ ყველა ნასწავლი მონაცემთა ტიპი და თითოეულს გვერდზე მიუწერეთ მაგალითი.
-# integer mag: 62
-# string mag:"gruop"
-# float mag: 5.3
-# bool mag:i am 9 years old true
---
+++ day 10/hw/hw.01.py
@@ -0,0 +1,41 @@
+#  3)გამოიყენე ყველა შედარებეთი ოპერატორები ორჯერ True და ორჯერ False რომ გამოვიდეს(<,>,==,<=,>=,!=)
+30 < 40 
+print(True)
+50 > 20
+print(True)
+50 > 60
+print(False)
+90 < 80
+print(False)
+50 == 50
+print(True)
+60 == 60
+print(True)
+60 == 90
+print(False)
+80 == 90
+print(False)
+40 <= 60
+print(True)
+50 <= 60
+print(True)
+40 <=30
+print(Felse)
+30 <=20
+print(FAlse)
+20 >= 20
+print(True)
+60 >=50
+print(True)
+50 >= 90
+print(False)
+60 >= 70
+print(False)
+30 != 50
+print(True)
+70 != 80
+print(True)
+40 !=40
+print(False)
+30 != 30
+print(False)
