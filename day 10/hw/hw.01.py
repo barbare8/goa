9374bf6775171cd5c67404c624ff52efe7a6615d
barbare8 <barbaretsivilashvili@gmail.com>
2025-03-14 16:34:13 +0400

    commit

--- day 10/hw/hw.01.py
+++ day 10/hw/hw.01.py
@@ -1,41 +1,44 @@
-#  3)გამოიყენე ყველა შედარებეთი ოპერატორები ორჯერ True და ორჯერ False რომ გამოვიდეს(<,>,==,<=,>=,!=)
-30 < 40 
-print(True)
-50 > 20
-print(True)
-50 > 60
-print(False)
-90 < 80
-print(False)
-50 == 50
-print(True)
-60 == 60
-print(True)
-60 == 90
-print(False)
-80 == 90
-print(False)
-40 <= 60
-print(True)
-50 <= 60
-print(True)
-40 <=30
-print(Felse)
-30 <=20
-print(FAlse)
-20 >= 20
-print(True)
-60 >=50
-print(True)
-50 >= 90
-print(False)
-60 >= 70
-print(False)
-30 != 50
-print(True)
-70 != 80
-print(True)
-40 !=40
-print(False)
-30 != 30
-print(False)
+# #  3)გამოიყენე ყველა შედარებეთი ოპერატორები ორჯერ True და ორჯერ False რომ გამოვიდეს(<,>,==,<=,>=,!=)
+# 30 < 40 
+# print(True)
+# 50 > 20
+# print(True)
+# 50 > 60
+# print(False)
+# 90 < 80
+# print(False)
+# 50 == 50
+# print(True)
+# 60 == 60
+# print(True)
+# 60 == 90
+# print(False)
+# 80 == 90
+# print(False)
+# 40 <= 60
+# print(True)
+# 50 <= 60
+# print(True)
+# 40 <=30
+# print(Felse)
+# 30 <=20
+# print(FAlse)
+# 20 >= 20
+# print(True)
+# 60 >=50
+# print(True)
+# 50 >= 90
+# print(False)
+# 60 >= 70
+# print(False)
+# 30 != 50
+# print(True)
+# 70 != 80
+# print(True)
+# 40 !=40
+# print(False)
+# 30 != 30
+# print(False)
+soil_moisture = 80
+soil_moisture = 120
+print(soil_moisture < 100)
