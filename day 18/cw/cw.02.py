9afd73bc7269c6568a3ca9315553802d19272e81
barbare8 <barbaretsivilashvili@gmail.com>
2025-04-10 21:49:02 +0400

    commit

---
+++ day 18/cw/cw.02.py
@@ -0,0 +1,3 @@
+# 2) შექმენით for loop, for loop ის მეშვეობით გამოიტანე 100 ჯერ GOA Best! ოღონდ ისე რომ მარცხნივ შეგეძლოს წაიკითხო მერამდენე GOA Best! არის , for i in range(100)
+for i in range(100):
+    print("goa best")
