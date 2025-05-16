d78fcc1ed00d330856df446cff08277e6f2e353f
barbare8 <barbaretsivilashvili@gmail.com>
2025-05-16 22:11:47 +0400

    commit

--- day 20/hw/hw.01.py
+++ day 20/hw/hw.01.py
@@ -1,5 +1,10 @@
-#1) დაწერე პროგრამა, რომელიც while ციკლით 3-ჯერ დაბეჭდავს სიტყვას „Hello“ 
-counter = 0
-while counter != 3:
-    print("hello")
-    counter += 1
+ #1) დაწერე პროგრამა, რომელიც while ციკლით 3-ჯერ დაბეჭდავს სიტყვას „Hello“ 
+# counter = 0
+# while counter != 3:
+#     print("hello")
+#     counter += 1
+password = "SecretWord"
+guess = input()
+while guess != password:  
+  guess = input() 
+print("Access Granted")
---
+++ day 28/cw/cw.03.py
@@ -0,0 +1,8 @@
+foods = ["თბილისი,გორი","ქუთაისი","barbare","ზესტაფონი",10,"nia","38","9","4","aleqsandre","FALSE","TRUE","FALSE"]
+user0 = int(input("enter your name"))
+user1 = int(input("enter your name"))
+user2 = int(input("enter your name"))
+foods [4] = user0
+foods [7] = user1
+foods [11] = user2
+print(foods)
