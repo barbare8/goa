652e63553e7c2fb2e2f37a0f1139cb18a9498312
barbare8 <barbaretsivilashvili@gmail.com>
2025-05-27 20:33:17 +0400

    commit

---
+++ day 30/hw/hw.03.py
@@ -0,0 +1,3 @@
+# 3)შექმენით სია სადაც შეინახავ 15 ცალ რიცვხს, შემდეგ დაპირნტეთ 10 ინდექსამდე ყველა ელემენტი slicing ით
+names = ["barbare","niako","aleqsandre","luka","ana","saba","salome","vako","taia","sandro","mariami","demetre","gabrieli","giorgi","gurami"]
+print(names[:10])
