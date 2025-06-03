6b2d3513bcf0ca3aa4922ea6e37a3bf1230e685e
barbare8 <barbaretsivilashvili@gmail.com>
2025-06-03 20:03:58 +0400

    commit

---
+++ day 32/cw/cw.01.py
@@ -0,0 +1,9 @@
+# 1)შექმენით 4 ცალი ცვლადი საიდან შენახავთ ტრინგერბს, პირველი სტრინგ დაპრინტეთ ყველა ასო დაპატარავებული, მეორე ყველა ასო გადიდებული, მესამე პირველი ასო გადიდებული მარტო, და მეოთხეში ყველა a ასო შეცვალეთ b თი(დაიხმარეთ რესურები)
+name = "barbare"
+surname = "tsivilashvili"
+programing_academi = "GOAL-oriented-academi"
+age = 9
+print(name.lower())
+print(surname.upper())
+print(programing_academi.capitalize())
+print(age)
---
+++ day 32/hw/hw.02.py
@@ -0,0 +1,8 @@
+# 2)შექმენით სია სადაც შენიახავთ იმდენ სტრინგს, რამდენი მეთოდიც ისწავლეთ(რესურსები), და ყვეალ სტრინგზე გამოიყენეთ ყველა მეთოდი
+animals=["cow","chicen","hen","dog","cat","mous"]
+print(animals[0].lower())
+print(animals[1].upper())
+print(animals[2].capitalize())
+print(animals[3].count("d"))
+print(animals[4].replace("c","b"))
+print(animals[5].title())
