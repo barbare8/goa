7966bd7f015c7b4d7620a54f4b5bcf2e9cdb981a
barbare8 <barbaretsivilashvili@gmail.com>
2025-06-07 18:47:09 +0400

    seqond commit

---
+++ day 34/cw/cw.01.py
@@ -0,0 +1,7 @@
+# 1)შექმენით სია 5 ცალი ელემტით,, შემდეგ მესამე ელემენტი ამოაგდეთ, და მეორე იდექსზე ჩაამატეთ შემდგომ 5 იანი, ბოლოს სიის ბოლოშ ჩაამატეთ "Gurami", და საბოლოოდ დაპრინტეთ ამ სიის სიგრძე 
+names = ["ana","mako","vako","nia","aleqsi"]
+names.pop(2)
+print(names)
+names = ["ana","mako","vako","nia","aleqsi"]
+names.append("gurami")
+print(names)
---
+++ day 34/hw/hw.01.py
@@ -0,0 +1,14 @@
+# )შექმენით ფუნქცია რომელიც დაპრინტავს "hello world", შემდეგ ეს ფუნქცია 10 ჯერ გამოიძახეთ
+def Gurami():
+    print("hello world")
+
+Gurami()
+Gurami()
+Gurami()
+Gurami()
+Gurami()
+Gurami()
+Gurami()
+Gurami()
+Gurami()
+Gurami()
