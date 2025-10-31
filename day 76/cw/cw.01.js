1d9ddf8e08fdb50320d0cab027ace06ec05a0d71
barbare8 <barbaretsivilashvili@gmail.com>
2025-10-31 21:08:16 +0400

    commit

---
+++ day 76/cw/cw.01.js
@@ -0,0 +1,35 @@
+// შემენით ობიექტები თავისი მახასიათებლებით და მეთოდებით:
+let car = {
+    model : "bmw",
+    color: "black",
+    maxSpeed: "120km/h",
+    year: 2017,
+    beep: function(){
+        console.log("beeeeeep")
+        car.beep()
+}
+}
+"------------------------------------------------------------------"
+let animal{
+    animal_name:"cat",
+    food_and_drink: 'milk',"fish",
+    maxSpeedspeed: 5km/h,
+}
+"-----------------------------------------------------------------------"
+let motorbike = {
+    model : "bmw",
+    color: "black",
+    maxSpeed: "120km/h",
+    year: 2017,
+}
+"-------------------------------------------------------------------------------"
+let computere = {
+    brand:"lenovo"
+    color: "silver",
+    year: 2017,}
+"----------------------------------------------------------------------------------"
+let phone = {
+    brand:"AIPHONE17",
+    color: "ORANGE",
+    year: 2025,
+}
