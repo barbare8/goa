ef2d1b8238da3e2b472c9ec44fff112ee39b2de1
barbare8 <barbaretsivilashvili@gmail.com>
2026-03-09 18:07:36 +0400

    commit

---
+++ day 109/cw/cw.01.js
@@ -0,0 +1,19 @@
+let cinema_tikets = 300
+while (tickets > 0) {
+    console.log("გაიყიდა ბილეთი");
+    tickets = tickets - 1;
+}
+console.log("ბოდიშით, მეტი ბილეთი აღარ გვაქვს");
+
+
+
+
+"----------------------------------------------------------------------"
+let tickets = 300;  
+
+for (let i = 1; i <= tickets; ) {
+    console.log("გაიყიდა ბილეთი");
+    i++
+}
+
+console.log("ბოდიშით, მეტი ბილეთი აღარ გვაქვს");
